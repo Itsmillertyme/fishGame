775158e063f9d4432ab2d89040503a3eb5b9501d
Lauren Rousell <laurenrousell@outlook.com>
2026-03-20 18:26:14 -0400

    Added backend for handling cards and cosmetics

--- Assets/PlayerData/PlayerData.cs
+++ Assets/PlayerData/PlayerData.cs
@@ -9,9 +9,17 @@ public class PlayerData
     public List<string> fishDiscoveredIds = new List<string>();
     public List<FishCatchEntry> fishCaughtPerSpecies = new List<FishCatchEntry>();
 
-    public List<string> cardIdsOwned = new List<string>();
+    public List<int> cardIdsOwned = new List<int>();
     public List<string> cosmeticIdsOwned = new List<string>();
     public List<string> areaIdsDiscovered = new List<string>();
+
+    public string equippedPlayerHatCosmeticId;
+    public string equippedPlayerShirtCosmeticId;
+    public string equippedPlayerBootsCosmeticId;
+
+    public int equippedRodCardId;
+    public int equippedReelCardId;
+    public int equippedLureCardId;
 }
 
 [Serializable]
